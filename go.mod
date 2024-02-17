e4dff22a0186d44dc7fcc073f1c61501e44441f4
Ravilochan <ravilochan2000@gmail.com>
2024-02-17 15:24:49 +0530

    SDL2 Hello World

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pong
 
 go 1.22.0
+
+require github.com/veandco/go-sdl2 v0.4.38
--- pong.go
+++ pong.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
 )
 
+const winWidth, winHeight int = 800,600
+
 func main() {
-	fmt.Println("Hello, world!")
+	// Added after EP06 to address macosx issues
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Hello World", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	 int32(winWidth),int32(winHeight), sdl.WINDOW_SHOWN)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer window.Destroy()
+	sdl.Delay(10000)
 }
